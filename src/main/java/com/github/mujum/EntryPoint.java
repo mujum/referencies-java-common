5c516a65ebf0c8a1fcaf10f7e128f009950f71ed
Владислав Прокудин <prokudin@glosav.ru>
2018-12-26 14:58:19 +0300

    RJC-4 uploadArchives gradle task added

--- src/main/java/com/github/mujum/EntryPoint.java
+++ src/main/java/com/github/mujum/EntryPoint.java
@@ -2,6 +2,12 @@ package com.github.mujum;
 
 import com.github.mujum.aggregate.AggregateEngine;
 
+/**
+ * @author mujum
+ *
+ * Точка входа в приложение, запускает модуль,
+ * выбирающий и запускающий различные тесты
+ */
 public class EntryPoint {
     public static void main(String[] args) {
         AggregateEngine.run();
--- src/main/java/com/github/mujum/aggregate/AggregateEngine.java
+++ src/main/java/com/github/mujum/aggregate/AggregateEngine.java
@@ -2,33 +2,43 @@ package com.github.mujum.aggregate;
 
 import com.github.mujum.current.CurrentHotTest;
 
+/**
+ * @author mujum
+ *
+ * Модуль, выбирающий какие модули будут запущены
+ * и запускающий их через их точку входа
+ */
 public class AggregateEngine {
 
     /**
      * All aviable modules
      */
-    public enum ModulesEnum {
+    private enum ModulesEnum {
         CurrentTest;
     }
 
     /**
      * Modules for run
      */
-    public static ModulesEnum currentModules[] = {
+    private static ModulesEnum currentModules[] = {
         ModulesEnum.CurrentTest
     };
 
     public static void run() {
         for(int index = 0; index < currentModules.length; index++) {
             System.out.println("\n#\nModule: " + currentModules[index].name() + " - started\n#\n#\n#\n#\n");
-            switch (currentModules[index]) {
-                case CurrentTest:
-                    new CurrentHotTest().test();
-                    break;
-                default:
-                    System.out.println("No code for this module");
-            }
+            entry(currentModules[index]);
             System.out.println("\n#\n#\n#\n#\nModule: " + currentModules[index].name() + " - completed\n#\n");
+        }
+    }
+
+    private static void entry(ModulesEnum module) {
+        switch (module) {
+            case CurrentTest:
+                new CurrentHotTest().test();
+                break;
+            default:
+                System.out.println("No code for this module");
         }
     }
 }
